03c84829d469cd522102e2568e5b70b50ba491cf
Denis Leon <dh92fr@ukr.net>
2025-12-10 16:54:53 +0200

    add spinner in submit-form

--- components/products/cart/orders/submit-form.tsx
+++ components/products/cart/orders/submit-form.tsx
@@ -1,11 +1,18 @@
 import { useFormStatus } from "react-dom";
+import { SpinnerCircularSplit } from "spinners-react";
+
 import styles from "./shipping-details.module.scss";
 
 export default function SubmitForm() {
   const status = useFormStatus();
 
   if (status.pending) {
-    return <p>Loading address...</p>;
+    return (
+      <p className={styles.loading}>
+        <span>Loading orders</span>
+        <SpinnerCircularSplit className={styles.diamond} />
+      </p>
+    );
   }
   return (
     <button className={styles.confirm} disabled={status.pending}>
