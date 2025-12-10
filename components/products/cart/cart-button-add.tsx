9d7efff67f95c77516fb7cf5223d623ff44e7fbd
Denis Leon <dh92fr@ukr.net>
2025-12-10 12:41:42 +0200

    add theme btn  in AddCart component

--- components/products/cart/cart-button-add.tsx
+++ components/products/cart/cart-button-add.tsx
@@ -7,12 +7,15 @@ import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { IProducts } from "@/lib/types";
 
 import "./cart-button-add.scss";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/themeContext";
 
 export default function AddToCart({ product }: { product: IProducts }) {
   const dispatch = useAppDispatch();
   const isInCart = useAppSelector((state) =>
     state.cartReducer.cart.some((item) => item.id === product.id)
   );
+  const { theme } = useContext(ThemeContext);
 
   const addProductButton = () => {
     dispatch(addProductToCart(product));
@@ -21,7 +24,7 @@ export default function AddToCart({ product }: { product: IProducts }) {
 
   return (
     <div
-      className={`cart-item-add cart-hover-add ${isInCart ? "disabled-cart-add" : ""}`}
+      className={`${theme} cart-item-add cart-hover-add ${isInCart ? "disabled-cart-add" : ""}`}
       onClick={addProductButton}
     >
       {isInCart ? "Thank You" : "ADD TO CART"}
