84f8985741d2c4edf7f62ae33a63be9504e6e104
Denis Leon <dh92fr@ukr.net>
2026-04-16 15:04:37 +0300

    add metadata in wishlist, reviews pages

--- app/(content)/reviews/page.tsx
+++ app/(content)/reviews/page.tsx
@@ -1,8 +1,14 @@
 import ReviewsWrapper from "@/components/reviews/reviews-wrapper";
 import { ReviewItem } from "@/components/reviews/types";
 import { getReviews } from "@/lib/api/reviews";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Reviews",
+  description: "Reviews page",
+};
 
 export default async function ReviewsPage() {
   const reviews = (await getReviews()) as ReviewItem[];
--- app/(content)/wishlist/page.tsx
+++ app/(content)/wishlist/page.tsx
@@ -1,5 +1,11 @@
 import WishListPage from "@/components/wishlist/wishlist";
+import { Metadata } from "next";
 
+export  const metadata: Metadata = {
+   title: "Wishlist",
+   description: "Wishlist page",
+ };
+ 
 export default function WishListProductsPage() {
   return (
     <div>
